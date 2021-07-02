815be9239d86e86a216443c72006cd919b8d8001
dynamic-daemon-637b <zl3087@columbia.edu>
2021-07-02 00:51:11 +0000

    ok

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ class App < Sinatra::Base
   get '/newteam' do
     erb :newteam
   end
-post '/team' do
+  
+  post '/team' do
   @name = params[:name]
   @coach = params[:coach]
   @pg =params[:pg]
